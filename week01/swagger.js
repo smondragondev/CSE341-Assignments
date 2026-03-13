17bfd4eca23a026533d4a4173aca18271b8bf64c
Sergio Mondragon <smondragnsilva@byupathway.edu>
2026-03-13 09:33:17 -0500

    Refactor host configuration in swagger.js for improved clarity

--- week01/swagger.js
+++ week01/swagger.js
@@ -1,12 +1,15 @@
 const swaggerAutogen = require('swagger-autogen')();
 const port = process.env.PORT || 3000;
+const domain = process.env.DOMAIN || 'localhost'
+const isDevelopment = process.env.ENV == 'development';
+const host = isDevelopment ?  `${domain}` : `${domain}:${port}`;
 
 const doc = {
     info: {
         title: 'Contacts API',
         description: 'This API is a CRUD of contacts'
     },
-    host: `localhost:${port}`
+    host: host
 }
 
 const outputFile = './swagger-output.json';
